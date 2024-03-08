5d4303fd34413323d1884bc6f382b6739e181736
Leonardo Teixeira <littledoge02@gmail.com>
2024-03-08 15:22:45 -0300

    :alembic: Benchmark changes

--- test/Byces.Calculator.Benchmarks/Benchmarks/ExpressionBenchmark.cs
+++ test/Byces.Calculator.Benchmarks/Benchmarks/ExpressionBenchmark.cs
@@ -11,7 +11,7 @@ public class ExpressionBenchmark
     private ICalculator _calculator;
 #pragma warning restore CS8618
 
-    [Params(CalculatorOptions.CacheExpressions)]
+    [Params(CalculatorOptions.Default, CalculatorOptions.CacheExpressions)]
     public CalculatorOptions Options;
         
     [GlobalSetup]
@@ -23,82 +23,82 @@ public class ExpressionBenchmark
     [Benchmark]
     public void NumberOnly()
     {
-        ExecuteDoubleCalculationMillionTimes("3");
+        ExecuteDoubleCalculationOneHundredThousandTimes("3");
     }
         
     [Benchmark]
     public void Random()
     {
-        ExecuteDoubleCalculationMillionTimes("random(1; 10)");
+        ExecuteDoubleCalculationOneHundredThousandTimes("random(1; 10)");
     }
 
     [Benchmark]
     public void SimpleCalculation()
     {
-        ExecuteDoubleCalculationMillionTimes("2 + 5 * 6");
+        ExecuteDoubleCalculationOneHundredThousandTimes("2 + 5 * 6");
     }
 
     [Benchmark]
     public void EulerPlusEulerPlusEuler()
     {
-        ExecuteDoubleCalculationMillionTimes("EULER + EULER + EULER");
+        ExecuteDoubleCalculationOneHundredThousandTimes("EULER + EULER + EULER");
     }
 
     [Benchmark]
     public void ComplexCalculation()
     {
-        ExecuteDoubleCalculationMillionTimes("2 ^ 2 + (4 + 5 * (2 √ 9))");
+        ExecuteDoubleCalculationOneHundredThousandTimes("2 ^ 2 + (4 + 5 * (2 √ 9))");
     }
 
     [Benchmark]
     public void HeavyCalculation()
     {
-        ExecuteDoubleCalculationMillionTimes("(2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9)))");
+        ExecuteDoubleCalculationOneHundredThousandTimes("(2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9))) / (2 ^ 2 + (4 + 5 * (2 √ 9)))");
     }
 
     [Benchmark]
     public void HeavyCalculationNoWhiteSpace()
     {
-        ExecuteDoubleCalculationMillionTimes("(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))");
+        ExecuteDoubleCalculationOneHundredThousandTimes("(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))/(2^2+(4+5*(2√9)))");
     }
 
     [Benchmark]
     public void ManyParenthesesCalculation()
     {
-        ExecuteDoubleCalculationMillionTimes("((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((2 + 2))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))");
+        ExecuteDoubleCalculationOneHundredThousandTimes("((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((2 + 2))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))");
     }
 
     [Benchmark]
     public void FactorialCalculation()
     {
-        ExecuteDoubleCalculationMillionTimes("fact2 + (fact2 + fact(fact2 + 2))");
+        ExecuteDoubleCalculationOneHundredThousandTimes("fact2 + (fact2 + fact(fact2 + 2))");
     }
 
     [Benchmark]
     public void SquareRootStringCalculation()
     {
-        ExecuteDoubleCalculationMillionTimes("SQRT9");
+        ExecuteDoubleCalculationOneHundredThousandTimes("SQRT9");
     }
 
     [Benchmark]
     public void SquareRootCharCalculation()
     {
-        ExecuteDoubleCalculationMillionTimes("√9");
+        ExecuteDoubleCalculationOneHundredThousandTimes("√9");
     }
 
     [Benchmark]
     public void AddFunctionCalculation()
     {
-        ExecuteDoubleCalculationMillionTimes("ADD(1;2;3)");
+        ExecuteDoubleCalculationOneHundredThousandTimes("ADD(1;2;3)");
     }
 
     [Benchmark]
     public void AddOperationCalculation()
     {
-        ExecuteDoubleCalculationMillionTimes("1+2+3");
+        ExecuteDoubleCalculationOneHundredThousandTimes("1+2+3");
     }
 
-    private void ExecuteDoubleCalculationMillionTimes(string expression)
+    private void ExecuteDoubleCalculationOneHundredThousandTimes(string expression)
     {
         for (int i = 0; i < 100_000; i++)
         {
